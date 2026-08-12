6f0b97f5802eb28ed2e7ed6110c176074b4e3b01
Chaudhary Naman Payal <namanpayal10@gmail.com>
2026-08-12 17:01:50 +0530

    Time: 3 ms (45.41%), Space: 9.6 MB (76.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string a, string b) {
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+
+        if(a!=b){
+            return false;
+        }
+        return true;
+        
+    }
+};
