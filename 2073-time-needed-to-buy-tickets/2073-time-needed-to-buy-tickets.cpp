f72c9d66773e49dfcaf2934997252884758e6231
Chaudhary Naman Payal <namanpayal10@gmail.com>
2026-08-15 11:04:11 +0530

    Time: 0 ms (100%), Space: 11 MB (43.42%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        queue<int>q;
+         queue<int>q;
         int time =0;
         for(int i=0; i<tickets.size(); i++){
             q.push(i);
