1b782cb79d103dd472a45cbe5a40b68c12f40faa
Chaudhary Naman Payal <namanpayal10@gmail.com>
2026-08-19 19:44:32 +0530

    Time: 31 ms (31.24%), Space: 139.3 MB (49.11%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,30 +2,26 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int>ans;
-        deque<int>d;
+        deque<int> dq;
 
-        for(int i=0; i<k-1; i++){
+        for(int i = 0; i < nums.size(); i++) {
 
-            if(d.empty()){
-                d.push_back(i);
+            // Remove elements outside current window
+            if(!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
             }
-            else{
-                while(!d.empty() && nums[i]>nums[d.back()]){
-                    d.pop_back();
-                }
-                d.push_back(i);
-            }
-        }
-        for(int i=k-1; i<nums.size(); i++){
-             while(!d.empty() && nums[i]>nums[d.back()]){
-                    d.pop_back();
-                }
-                d.push_back(i);
 
-                if(d.front()<=i-k){
-                    d.pop_front();
-                }
-                ans.push_back(nums[d.front()]);
+            // Remove smaller elements
+            while(!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            // Add maximum of current window
+            if(i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
         }
         return ans;
     }
