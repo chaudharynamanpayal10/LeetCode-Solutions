96647d53501b65fe257c7259db0623dce8ca20f0
Chaudhary Naman Payal <namanpayal10@gmail.com>
2026-09-11 19:24:44 +0530

    Time: 15 ms (25.26%), Space: 30.9 MB (51.66%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -7,15 +7,12 @@ public:
             for (int j = 0; j < digits.size(); j++) {
                 for (int k = 0; k < digits.size(); k++) {
 
-                    // Same index cannot be used twice
                     if (i == j || j == k || i == k)
                         continue;
 
-                    // First digit cannot be 0
                     if (digits[i] == 0)
                         continue;
 
-                    // Last digit must be even
                     if (digits[k] % 2 != 0)
                         continue;
 
